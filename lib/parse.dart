2efd734bf64b33b5a63b32fbff357e3933001cb2
侑介 <spark9487111@gmail.com>
2024-12-14 09:53:10 +0800

    rename

--- lib/parse.dart
+++ lib/parse.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:beautiful_soup_dart/beautiful_soup.dart';
 
-Future<List<String>> parse(String url) async{
+Future<List<String>> parse(String url) async {
   List<String> list = [];
-
+  url =
+      "https://anime1.me/category/2023%E5%B9%B4%E6%98%A5%E5%AD%A3/mix-%E7%AC%AC%E4%BA%8C%E5%AD%A3";
   RegExp esp = RegExp(r'anime1.me/[0-9]');
   RegExp season = RegExp(r'anime1.me/category/(.*?)');
   if (esp.hasMatch(url)) {
@@ -23,13 +24,14 @@ Future<List<String>> getEpisode(String url) async {
   var u = Uri.parse(Uri.decodeFull(url));
   Map<String, String> headers = getHeader();
   var response = await http.get(u, headers: headers);
-  
+
   BeautifulSoup soup = BeautifulSoup(response.body);
 
   var h2 = soup.findAll('h2', class_: 'entry-title');
 
   for (var element in h2) {
-    var url = element.find('a', attrs: {'rel': "bookmark"})!.getAttrValue('href')!;
+    var url = element
+        .find('a', attrs: {'rel': "bookmark"})!.text; //getAttrValue('href')!;
     urls.add(url);
   }
 
@@ -39,5 +41,7 @@ Future<List<String>> getEpisode(String url) async {
     urls.addAll(await getEpisode(nextUrl));
   }
 
-  return urls.reversed.toList();
+  var title = soup.find('h1', class_: 'page-title')!.text;
+  urls.add(title);
+  return urls;
 }
