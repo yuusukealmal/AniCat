8dd16f892ff101197dfd4da095947412f85fc266
侑介 <spark9487111@gmail.com>
2025-01-09 21:08:57 +0800

    refactor: reduce repeat code

--- lib/main.dart
+++ lib/main.dart
@@ -319,7 +319,6 @@ class FileListScreen extends StatefulWidget {
 class FileListScreenState extends State<FileListScreen> with _Load, _Rotate {
   List<FileSystemEntity> _files = [];
   Future<List<String>>? _fileCacheMap;
-  int _progress = 0;
 
   @override
   void initState() {
@@ -348,22 +347,58 @@ class FileListScreenState extends State<FileListScreen> with _Load, _Rotate {
       var path = "${cache.path}/$hash.png";
       if (!await File(path).exists()) {
         debugPrint("Downloading ${file.path}");
-        await _getThumbnail(file as File).then((value) => {
-              hashMap.add(value),
-              setState(() {
-                _progress++;
-              })
-            });
+        await _getThumbnail(file as File).then((value) => {hashMap.add(value)});
       } else {
         debugPrint("Cached ${file.path}");
         hashMap.add(path);
-        setState(() {
-          _progress++;
-        });
       }
     }
-    debugPrint("Map ${hashMap.toString()}");
     return hashMap;
+  }
+
+  ListView _fileListView([List<String>? cacheThumbnail]) {
+    return ListView.builder(
+      itemCount: _files.length,
+      itemBuilder: (context, index) {
+        final file = _files[index] as File;
+        final fileName = file.path.split('/').last;
+        return ListTile(
+          title: Text(fileName),
+          subtitle: Text(getFileSize(file.lengthSync())),
+          leading: {
+                'mp4': cacheThumbnail?[index] != null &&
+                        cacheThumbnail?[index] != ""
+                    ? Image(image: FileImage(File(cacheThumbnail![index])))
+                    : const Icon(Icons.video_file),
+                'mkv': const Icon(Icons.video_file),
+                'webm': const Icon(Icons.video_file),
+                'jpg': const Icon(Icons.image),
+                'png': const Icon(Icons.image),
+                'jpeg': const Icon(Icons.image),
+                'gif': const Icon(Icons.gif),
+                'mp3': const Icon(Icons.music_note),
+                'wav': const Icon(Icons.music_note),
+                'aac': const Icon(Icons.music_note),
+                'ogg': const Icon(Icons.music_note),
+                'm4a': const Icon(Icons.music_note),
+                'flac': const Icon(Icons.music_note),
+                'mp4v': const Icon(Icons.video_file),
+                'mov': const Icon(Icons.video_file),
+                'wmv': const Icon(Icons.video_file),
+                'avi': const Icon(Icons.video_file),
+              }[fileName.split(".").last] ??
+              Icon(Icons.insert_drive_file),
+          onTap: () {
+            debugPrint('Tapped file: $fileName');
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => VideoPlayerScreen(filePath: file.path)),
+            );
+          },
+        );
+      },
+    );
   }
 
   @override
@@ -387,70 +422,22 @@ class FileListScreenState extends State<FileListScreen> with _Load, _Rotate {
               future: _fileCacheMap,
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
+                  return _fileListView();
+                }
+                if (snapshot.hasError) {
                   return Center(
                       child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      const CircularProgressIndicator(),
-                      Text(
-                          "Progress Thumbnail Cache $_progress/${_files.length}")
+                      const Icon(Icons.error_outline_rounded),
+                      Text("Progress Thumbnail Cache Failed")
                     ],
                   ));
                 }
                 if (snapshot.connectionState == ConnectionState.done) {
-                  return ListView.builder(
-                    itemCount: _files.length,
-                    itemBuilder: (context, index) {
-                      final file = _files[index] as File;
-                      final fileName = file.path.split('/').last;
-                      return ListTile(
-                        title: Text(fileName),
-                        subtitle: Text(getFileSize(file.lengthSync())),
-                        leading: {
-                              'mp4': snapshot.data![index] != ""
-                                  ? Image(
-                                      image: FileImage(
-                                          File(snapshot.data![index])))
-                                  : const Icon(Icons.video_file),
-                              'mkv': const Icon(Icons.video_file),
-                              'webm': const Icon(Icons.video_file),
-                              'jpg': const Icon(Icons.image),
-                              'png': const Icon(Icons.image),
-                              'jpeg': const Icon(Icons.image),
-                              'gif': const Icon(Icons.gif),
-                              'mp3': const Icon(Icons.music_note),
-                              'wav': const Icon(Icons.music_note),
-                              'aac': const Icon(Icons.music_note),
-                              'ogg': const Icon(Icons.music_note),
-                              'm4a': const Icon(Icons.music_note),
-                              'flac': const Icon(Icons.music_note),
-                              'mp4v': const Icon(Icons.video_file),
-                              'mov': const Icon(Icons.video_file),
-                              'wmv': const Icon(Icons.video_file),
-                              'avi': const Icon(Icons.video_file),
-                            }[fileName.split('.').last] ??
-                            const Icon(Icons.insert_drive_file),
-                        onTap: () {
-                          debugPrint('Tapped file: $fileName');
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    VideoPlayerScreen(filePath: file.path)),
-                          );
-                        },
-                      );
-                    },
-                  );
+                  return _fileListView(snapshot.data!);
                 }
-                return Center(
-                    child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Icon(Icons.error_outline_rounded),
-                    Text("Progress Thumbnail Cache Failed")
-                  ],
-                ));
+                return SizedBox.shrink();
               })),
     );
   }
