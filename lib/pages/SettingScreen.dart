52696276610246a8264e5b6b3cde2922430648d1
侑介 <spark9487111@gmail.com>
2025-01-16 02:06:57 +0800

    feat: add shared preferences

--- lib/pages/SettingScreen.dart
+++ lib/pages/SettingScreen.dart
@@ -10,7 +10,7 @@ class SettingScreen extends StatefulWidget {
 }
 
 class _SettingScreenState extends State<SettingScreen> {
-  int selectedColor = SharedPreferencesHelper.getInt("Home.Color")!;
+  int? selectedColor;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,58 +27,98 @@ class _SettingScreenState extends State<SettingScreen> {
       body: RefreshIndicator(
         onRefresh: () => Future.value(),
         child: ListView.builder(
-          itemCount: 1,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text("Change Theme Color"),
-              trailing: IconButton(
-                icon: const Icon(Icons.color_lens),
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) => AlertDialog(
-                      title: const Text("Change Theme Color"),
-                      content: SingleChildScrollView(
-                        child: ColorPicker(
-                          pickerColor: Color(selectedColor),
-                          onColorChanged: (Color color) {
-                            setState(() {
-                              selectedColor = color.value;
-                            });
-                          },
+            itemCount: 2,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return ListTile(
+                  title: Text("Change Theme Color"),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.color_lens),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) => AlertDialog(
+                          title: const Text("Change Theme Color"),
+                          content: SingleChildScrollView(
+                            child: ColorPicker(
+                              pickerColor: Color(selectedColor ??
+                                  Theme.of(context).colorScheme.primary.value),
+                              onColorChanged: (Color color) {
+                                setState(() {
+                                  selectedColor = color.value;
+                                });
+                              },
+                            ),
+                          ),
+                          actions: [
+                            TextButton(
+                              child: const Text('Cancel'),
+                              onPressed: () => Navigator.of(context).pop(),
+                            ),
+                            TextButton(
+                              child: const Text('OK'),
+                              onPressed: () async {
+                                await SharedPreferencesHelper.setInt(
+                                    "Home.Color",
+                                    selectedColor ??
+                                        Theme.of(context)
+                                            .colorScheme
+                                            .primary
+                                            .value);
+                                setState(() {
+                                  final ThemeData themeData = ThemeData(
+                                    colorScheme: ColorScheme.fromSeed(
+                                      seedColor: Color(selectedColor ??
+                                          Theme.of(context)
+                                              .colorScheme
+                                              .primary
+                                              .value),
+                                    ),
+                                  );
+                                  Theme.of(super.context).copyWith(
+                                    colorScheme: themeData.colorScheme,
+                                  );
+                                });
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
                         ),
-                      ),
-                      actions: [
-                        TextButton(
-                          child: const Text('Cancel'),
-                          onPressed: () => Navigator.of(context).pop(),
-                        ),
-                        TextButton(
-                          child: const Text('OK'),
-                          onPressed: () async {
-                            await SharedPreferencesHelper.setInt(
-                                "Home.Color", selectedColor);
-                            setState(() {
-                              final ThemeData themeData = ThemeData(
-                                colorScheme: ColorScheme.fromSeed(
-                                  seedColor: Color(selectedColor),
-                                ),
-                              );
-                              Theme.of(super.context).copyWith(
-                                colorScheme: themeData.colorScheme,
-                              );
-                            });
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            );
-          },
-        ),
+                      );
+                    },
+                  ),
+                );
+              }
+              if (index == 1) {
+                return ListTile(
+                    title: Text("Clear Shared Preferences"),
+                    trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () => showDialog(
+                            context: context,
+                            builder: (BuildContext context) => AlertDialog(
+                                    title:
+                                        const Text("Clear Shared Preferences"),
+                                    content: const Text(
+                                        "Are you sure you want to clear shared preferences?"),
+                                    actions: [
+                                      TextButton(
+                                        child: const Text("Cancel"),
+                                        onPressed: () =>
+                                            Navigator.of(context).pop(),
+                                      ),
+                                      TextButton(
+                                        child: const Text("Clear"),
+                                        onPressed: () async {
+                                          await SharedPreferencesHelper
+                                              .delete();
+                                          Navigator.of(context).pop();
+                                        },
+                                      )
+                                    ]))));
+              }
+              return const SizedBox.shrink();
+            }),
       ),
     );
   }
